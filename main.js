0ff0003e4bc7611ddd47b860580a1c4880e56bec
ELDADBRHANO <ebrhano@gmail.com>
2022-06-11 23:51:26 +0300

    saturday_commit

--- main.js
+++ main.js
@@ -1,75 +1,89 @@
+function checkValidation() {
+  if(checkBirthDate()&&checkFirstLatter()&&checkPrivateName&&checkLastName&&checkPasswords&&checkPhone){
+    return true
+  }
+  return false
+}
+
+
+
+
 // 1
-// function checkPrivateName() {
+function checkPrivateName() {
 
-//   var reg = /^(?=.*[A-Za-z])/;
+  var reg = /^(?=.*[A-Za-z])/;
 
-//   if(reg.test(firstName.value)){
-//     return true
-//   }
-
-//   alert("dont work")
-//   return false
-// }
+  if(reg.test(firstName.value)){
+    return true
+  }
+  return false
+}
 
 
-
+function checkFirstLatter() {
+  if(firstName.value==firstName.value.toUpperCase()){
+    return true
+  }
+  return false
+}
 
 
 // 2
-// function checkLastName() {
+function checkLastName() {
 
-//   var reg =/^(?=.*[A-Za-z])/;
+  var reg =/^(?=.*[A-Za-z])/;
 
-//   if(reg.test(lastName.value)){
-//     alert("true")
-//     console.log(lastName);
-//     return true
-//   }
+  if(reg.test(lastName.value)){
+    alert("true")
+    console.log(lastName);
+    return true
+  }
 
-//   console.log(lastName);
-//   alert("no")
-//   return false
+  console.log(lastName);
+  alert("no")
+  return false
 
-// }
-
-
-
-// function checkPasswords() {
-
-//   var strongRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,12})/;
-
-//   if(pass1.value==pass2.value){
-//     if(strongRegex.test(pass2.value)){
-//       alert("true")
-//       return true
-//     }
-
-//     alert("password too weak, try again and add symbols.")
-//     return false
-//   }
-
-//   alert("password do not match.")
-//   return false
-// }
+}
 
 
 
-// function checkPhone() {
+function checkPasswords() {
 
-//   var phoneRegex = /^\d{10}/;
+  var strongRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,12})/;
 
-//   if(phoneRegex.test(phone.value)){
-//     return true
-//   }
+  if(pass1.value==pass2.value){
+    if(strongRegex.test(pass2.value)){
+      alert("true")
+      return true
+    }
 
-//   return false
-// }
+    alert("password too weak, try again and add symbols.")
+    return false
+  }
+
+  alert("password do not match.")
+  return false
+}
+
+
+
+function checkPhone() {
+
+  var phoneRegex = /^\d{10}/;
+
+  if(phoneRegex.test(phone.value)){
+    return true
+  }
+
+  return false
+}
 
 
 function checkBirthDate() {
- var RegExp= /(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$/;
- if(RegExp.test(age.value)){
-   return true
- }
- return false
+var nowDate = new Date().getFullYear();
+var userAge=parseInt(age.value.substr(0,4));
+if(nowDate - userAge > 18){
+  return true
+}
+return false
 }
